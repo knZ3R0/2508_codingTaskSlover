25fb899c4493992a0b052cdd614fb799b224eefc
darulec1 at red-hive <darulec1@gmail.com>
2025-08-07 23:32:25 +0200

    ok

--- 02_usingSolver_test01/src/main/java/pl/darulec/Result.java
+++ 02_usingSolver_test01/src/main/java/pl/darulec/Result.java
@@ -7,40 +7,7 @@ import java.net.http.HttpResponse;
 import java.util.*;
 
 public class Result {
-    public static List<String> asteroidMonitor(int year, String pha) {
-        List<String> result = new ArrayList<>();
-        try {
-            String url = "https://jsonmock.hackerrank.com/api/asteroids/search?page=1";
-            if (year > 0) url += "&discovery_date=" + year;
-            if (pha != null) url += "&pha=" + pha;
-
-            HttpResponse<String> response = HttpClient.newHttpClient()
-                .send(HttpRequest.newBuilder().uri(URI.create(url)).build(), 
-                      HttpResponse.BodyHandlers.ofString());
-            
-            String json = response.body();
-            int dataStart = json.indexOf("\"data\":[") + 8;
-            String data = json.substring(dataStart, json.lastIndexOf("]"));
-            
-            // Proste wydobycie designation i period_yr
-            for (String item : data.split("\\},\\{")) {
-                if (item.contains("\"designation\":")) {
-                    int start = item.indexOf("\"designation\":\"") + 14;
-                    int end = item.indexOf("\"", start);
-                    if (start > 13 && end > start) {
-                        result.add(item.substring(start, end));
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Błąd: " + e.getMessage());
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        for (String asteroida : asteroidMonitor(2010, "Y")) {
-            System.out.println(asteroida);
-        }
+        System.out.println("hello :)");
     }
 }

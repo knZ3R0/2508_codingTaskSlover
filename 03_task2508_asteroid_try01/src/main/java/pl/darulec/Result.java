120e7b552fb59b462bfeff059a96169633b49286
darulec1 at red-hive <darulec1@gmail.com>
2025-08-07 23:34:41 +0200

    ok

---
+++ 03_task2508_asteroid_try01/src/main/java/pl/darulec/Result.java
@@ -0,0 +1,13 @@
+package pl.darulec;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+
+public class Result {
+    public static void main(String[] args) {
+        System.out.println("hello :)");
+    }
+}
